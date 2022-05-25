ffdb102f8f7dc85b43066a4af9c27553c8f1acc5
ihtada lutfi ramadani <ihtadalutfi00@gmail.com>
2022-05-25 11:52:16 +0700

    Asessment 2

---
+++ app/src/main/java/com/d3ifcool44/pendaminghidupmu/ui/AboutFragment.kt
@@ -0,0 +1,7 @@
+package com.d3ifcool44.pendaminghidupmu.ui
+
+import androidx.fragment.app.Fragment
+import com.d3ifcool44.pendaminghidupmu.R
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+}
--- app/src/main/java/com/d3ifcool44/pendaminghidupmu/ui/HitungFragment.kt
+++ app/src/main/java/com/d3ifcool44/pendaminghidupmu/ui/HitungFragment.kt
@@ -2,9 +2,7 @@ package com.d3ifcool44.pendaminghidupmu.ui
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -22,58 +20,74 @@ class HitungFragment : Fragment() {
     private val viewModel: MainViewModel by lazy {
         ViewModelProvider(requireActivity())[MainViewModel::class.java]
     }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-
-        binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
-        return binding.root
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment
+            )
+            return true
+        }
+            return super.onOptionsItemSelected(item)
     }
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            binding.buttonTotal.setOnClickListener { hitungUang() }
-            binding.saranButton.setOnClickListener { viewModel.mulaiNavigasi() }
-            viewModel.getHasilPH().observe(requireActivity(), { showResult(it) })
-            viewModel.getNavigasi().observe(viewLifecycleOwner,{
-                if (it == null ) return@observe
-                findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment2(it))
-                viewModel.selesaiNavigasi()
-            })
+
+
+            override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+                binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
+                setHasOptionsMenu(true)
+                return binding.root
+            }
+
+            override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+                binding.buttonTotal.setOnClickListener { hitungUang() }
+                binding.saranButton.setOnClickListener { viewModel.mulaiNavigasi() }
+                viewModel.getHasilPH().observe(requireActivity(), { showResult(it) })
+                viewModel.getNavigasi().observe(viewLifecycleOwner,{
+                    if (it == null ) return@observe
+                    findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment2(it))
+                    viewModel.selesaiNavigasi()
+                })
+            }
+
+            private fun hitungUang() {
+                val pemasukan = binding.uangMasuk.text.toString().toFloat()
+                if (TextUtils.isEmpty(pemasukan.toString())) {
+                    Toast.makeText(context, "Harus Di isi", Toast.LENGTH_LONG).show()
+                    return
+                }
+                val pengeluaran = binding.uangKeluar.text.toString().toFloat()
+                if (TextUtils.isEmpty(pengeluaran.toString())) {
+                    Toast.makeText(context, "Harus Di Isi", Toast.LENGTH_LONG).show()
+                }
+                val hemat = binding.totalUangText.text.toString().toBoolean()
+
+                viewModel.hitungUang(
+                    pemasukan.toFloat(),
+                    pengeluaran.toFloat(),
+                )
+
+            }
+
+            private fun showResult(result: HasilPH?){
+                if(result == null) return
+                binding.totalUangText.text = getString(R.string.kategori_x,getKategoriLabel(result.kategori))
+                binding.saranButton.visibility = View.VISIBLE
+            }
+
+            private fun getKategoriLabel (kategori:KategoriPH): String{
+                val stringRes = when (kategori){
+                    KategoriPH.HEMAT -> R.string.hemat
+                    KategoriPH.BOROS -> R.string.boros
+                }
+                return getString(stringRes)
+            }
+
         }
-
-    private fun hitungUang() {
-        val pemasukan = binding.uangMasuk.text.toString().toFloat()
-        if (TextUtils.isEmpty(pemasukan.toString())) {
-            Toast.makeText(context, "Harus Di isi", Toast.LENGTH_LONG).show()
-            return
-        }
-        val pengeluaran = binding.uangKeluar.text.toString().toFloat()
-        if (TextUtils.isEmpty(pengeluaran.toString())) {
-            Toast.makeText(context, "Harus Di Isi", Toast.LENGTH_LONG).show()
-        }
-        val hemat = binding.totalUangText.text.toString().toBoolean()
-
-        viewModel.hitungUang(
-            pemasukan.toFloat(),
-            pengeluaran.toFloat(),
-        )
-
-        }
-
-    private fun showResult(result: HasilPH?){
-        if(result == null) return
-        binding.totalUangText.text = getString(R.string.kategori_x,getKategoriLabel(result.kategori))
-        binding.saranButton.visibility = View.VISIBLE
-    }
-
-    private fun getKategoriLabel (kategori:KategoriPH): String{
-        val stringRes = when (kategori){
-            KategoriPH.HEMAT -> R.string.hemat
-            KategoriPH.BOROS -> R.string.boros
-        }
-        return getString(stringRes)
-    }
-
-}
 
 private fun MainViewModel.hitungUang(pemasukan: Float, pengeluaran: Float) {
 
